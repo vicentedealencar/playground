0895ae6b7a17a0a3c386d3d1ca06903eef466bd1
Vicente de Alencar <vicentedealencar@gmail.com>
2013-01-03 17:32:51 -0200

    Hello Nancy

---
+++ HelloNancy/HelloNancy/MyModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloNancy
+{
+    public class MyModule : NancyModule
+    {
+        public MyModule() {
+            Get["/"] = p => "Hello, I'm Nancy";
+        }
+    }
+}
