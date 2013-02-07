b48383629483041e7faa7236cb14644f7b1d1684
Vicente de Alencar <vicentedealencar@gmail.com>
2013-02-06 22:52:42 -0200

    bruteforce innitial

---
+++ bruteforce/app.js
@@ -0,0 +1,43 @@
+// var solved = bruteforce('abc', function(possible){ return possible == 'babaca'; });
+
+// bruteforce( /[a-z0-9]/g, 
+//   function(possible){ 
+//     post('fb.com', {login: 'mark', pass: possible}, 
+//       function(worked){ return worked; }) 
+//   }, 
+//   function(pass){
+//     console.log('marks pass: ' + pass);
+//   });
+
+var bruteforce = function(alphabet, test) {
+	var max = 8;
+	var letters = alphabet.split('');
+
+	return recurse('');
+
+	function recurse (currentTry) {
+		console.log(currentTry);
+
+		if(test(currentTry)) {
+			return currentTry;
+		}
+
+		if(currentTry.length >= max) {
+			return null;
+		}
+
+		for (var i = letters.length - 1; i >= 0; i--) {
+			var result = recurse(currentTry + letters[i]);
+			if(result) {
+				return result;
+			}
+		};
+
+		return null;
+	}
+
+};
+
+var solved = bruteforce('abc', function(possible){ return possible == 'babaca'; });
+console.log("solved!!!!!");
+console.log(solved);
