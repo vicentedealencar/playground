964ad043399066bebdf001680df8ce825f71f1ee
Vicente de Alencar <vicentedealencar@gmail.com>
2013-03-05 20:40:49 -0300

    intro to ejs

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,29 +1,47 @@
-var express = require('express');
-
 var db = {
-	users: [
+	users: 
+	{
+		v57:
 		{
 			email: 'v57@gmail.com',
 			pass: '1234',
-			dashboards: [
+			dashboards: 
+			{
+				work:
 				{
-					widgets: [
+					widgets: 
+					{
+						clock:
 						{
-							name: 'clock',
-							config: {color: 'black'}
+							color: 'black'
 						}
-					]
+					}
 				}
-			]
+			}
 		}
-	]
+	}
 };
 
-
+var express = require('express');
 var app = express();
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
 
 app.get('/', function (req, res) {
-	res.send('Hello World');
+	res.render('index', {
+		title: 'Hello!!'
+	});
+});
+
+app.get('/:user/:dashboard', function(req, res) {
+	//TODO: verify if breaks
+	var widgets = db.users[req.params.user].dashboards[req.params.dashboard].widgets;
+
+	Object.keys(widgets).forEach(function (key) {
+		res.write(key +' - '+JSON.stringify(widgets[key]));
+	});
+
+	res.end();
 });
 
 app.listen(3000);
