2de2f0e3957e632c3a5e9078725a1c76da1036dd
Vicente de Alencar <vicentedealencar@gmail.com>
2013-03-15 16:27:52 -0300

    getting photo

--- woot-api/app.js
+++ woot-api/app.js
@@ -6,8 +6,12 @@ jsdom.env(
   function (errors, window) {
     var price = window.$('#todays-deal .price').text().trim(),
         deal = window.$('#todays-deal .fn').text().trim(),
-        link = window.$('#todays-deal .wantone').attr('href');
+        link = window.$('#todays-deal .wantone').attr('href'),
+        photo = window.$('#todays-deal .photo').attr('src');
         
-    console.log("Today's Woot deal is ", deal, "! Just: ", price, " Find more here: ", link);
+    console.log("Today's Woot deal is: ", deal);
+    console.log("price: ", price);
+    console.log("link: ", link);
+    console.log("photo: ", photo);
   }
 );
