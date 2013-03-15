3e5a3154c586f927fc5fa6fe5133d29952fe2826
Vicente de Alencar <vicentedealencar@gmail.com>
2013-03-15 17:02:55 -0300

    loks like a package

--- woot-api/app.js
+++ woot-api/app.js
@@ -1,17 +1,46 @@
 var jsdom = require("jsdom");
 
-jsdom.env(
-  "http://www.woot.com",
-  ["http://code.jquery.com/jquery.js"],
-  function (errors, window) {
-    var price = window.$('#todays-deal .price').text().trim(),
-        deal = window.$('#todays-deal .fn').text().trim(),
-        link = window.$('#todays-deal .wantone').attr('href'),
-        photo = window.$('#todays-deal .photo').attr('src');
-        
-    console.log("Today's Woot deal is: ", deal);
-    console.log("price: ", price);
-    console.log("link: ", link);
-    console.log("photo: ", photo);
-  }
-);
+var woot = function(subdomain, callback, errorCallback){
+	var onError = errorCallback || function(){};
+
+	if (arguments.length == 1) {
+		callback = arguments[0];
+		subdomain = 'www';
+	}
+	
+	jsdom.env(
+		'http://' + subdomain + '.woot.com',
+		['http://code.jquery.com/jquery.js'],
+		function (errors, window) {
+			if (errors) {
+				onError(errors);
+			}
+
+			callback({
+				deal: window.$('#todays-deal .fn').text().trim(),
+				price: window.$('#todays-deal .price').text().trim(),
+				link: window.$('#todays-deal .wantone').attr('href'),
+				photo: window.$('#todays-deal .photo').attr('src')
+			});
+		}
+	);
+
+};
+
+module.exports = woot;
+
+// woot('tech', function (ret) {
+// 	console.log('TECH');
+// 	console.log("deal: \t", ret.deal);
+// 	console.log("price: \t", ret.price);
+// 	console.log("link: \t", ret.link);
+// 	console.log("photo: \t", ret.photo);
+// });
+
+// woot(function (ret) {
+// 	console.log('DEFAULT');
+// 	console.log("deal: \t", ret.deal);
+// 	console.log("price: \t", ret.price);
+// 	console.log("link: \t", ret.link);
+// 	console.log("photo: \t", ret.photo);
+// });
